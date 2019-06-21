825aa2d4201b5a3d66ab4acc6031e01797b90526
ZHICHENG CHEN <alvin.chen@incrte.com>
2019-06-21 13:19:36 +0800

    模擬修改MainActivity

--- app/src/main/java/com/playground/buildcicd/MainActivity.java
+++ app/src/main/java/com/playground/buildcicd/MainActivity.java
@@ -1,13 +1,26 @@
 package com.playground.buildcicd;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+
+        tvHello.setBackgroundColor(Color.BLUE);
+
+    }
+
+    private void initUI(){
+        tvHello = findViewById(R.id.tvHello);
     }
 }
